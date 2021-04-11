cf8f0714804eba39033154f00e3cfda1cd802ad5
Alessandro Alterno <alternonolimit@gmail.com>
2021-04-11 23:52:36 +0200

    Created Message model

--- lib/services/models/message.dart
+++ lib/services/models/message.dart
@@ -1,3 +1,11 @@
-class Message {
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Message {
+  final DocumentReference convId;
+  final String content;
+  final DocumentReference sender;
+  final Timestamp timestamp;
+
+  Message({ required this.content, required this.convId, required this.sender,
+    required this.timestamp});
 }
