e0f0cb1855472fc02a209b6edb2da55b4840738d
Alessandro Alterno <alternonolimit@gmail.com>
2021-04-11 23:49:55 +0200

    Created Action model

--- lib/services/models/action.dart
+++ lib/services/models/action.dart
@@ -1,3 +1,20 @@
- class Action {
+ import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Action {
+ final String name;
+ final String city;
+ final String postalCode;
+ final String address;
+ final String description;
+ final String type;
+
+ final Timestamp startDate;
+ final Timestamp endDate;
+
+ final DocumentReference assosId;
+
+ Action({ required this.name, required this.city, required this.postalCode,
+  required this.address, required this.description,
+  required this.type, required this.startDate, required this.endDate,
+  required this.assosId});
  }
