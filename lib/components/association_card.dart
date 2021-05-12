37b38f132ed7d8ef78c033edce67b067ae9cf209
Alessandro Alterno <alternonolimit@gmail.com>
2021-05-12 15:22:37 +0200

    Added displayed informations to association_card.dart

--- lib/components/association_card.dart
+++ lib/components/association_card.dart
@@ -1,4 +1,5 @@
 import 'package:assosnation_app/services/models/association.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AssociationCard extends StatelessWidget {
@@ -10,7 +11,37 @@ class AssociationCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Stack(
-        children: [Image.network(association.banner), Text(association.name)],
+        children: [
+          Image.network(
+            association.banner,
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.height * 0.8,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(association.name),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(association.city),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(association.postalCode),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
