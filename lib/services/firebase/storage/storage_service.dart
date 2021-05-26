8b9a38c9a4a6a889a153fc4b97a7c8ed5574c86a
Alessandro Alterno <alternonolimit@gmail.com>
2021-05-26 23:57:46 +0200

    Can now retrieve the default image for association

--- lib/services/firebase/storage/storage_service.dart
+++ lib/services/firebase/storage/storage_service.dart
@@ -1,6 +1,9 @@
 import 'package:assosnation_app/services/interfaces/storage_interface.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class StorageService extends StorageInterface {
+  FirebaseStorage _storage = FirebaseStorage.instance;
+
   @override
   Future getBannerByAssociation() {
     // TODO: implement getBannerByAssociation
@@ -8,14 +11,34 @@ class StorageService extends StorageInterface {
   }
 
   @override
-  Future getDefaultAssocaitonBanner() {
-    // TODO: implement getDefaultAssocaitonBanner
-    throw UnimplementedError();
+  Future<String> getDefaultAssocaitonBannerUrl() async {
+    try {
+      Reference ref = _storage
+          .ref()
+          .child('associations_banners')
+          .child('default_assos.jpg');
+      final _imageUrl = await ref.getDownloadURL();
+      return _imageUrl;
+    } on FirebaseException catch (e) {
+      return Future.error("Cannot find the default image url");
+    }
   }
 
   @override
   Future getImageByPost() {
     // TODO: implement getImageByPost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future getBannerByName(String path) {
+    // TODO: implement getBannerByName
+    throw UnimplementedError();
+  }
+
+  @override
+  Future uploadBannerToStorage(banner) {
+    // TODO: implement uploadBannerToStorage
     throw UnimplementedError();
   }
 }
--- lib/services/interfaces/storage_interface.dart
+++ lib/services/interfaces/storage_interface.dart
@@ -1,7 +1,7 @@
-import 'package:assosnation_app/services/models/association.dart';
-
 abstract class StorageInterface {
   Future getBannerByAssociation();
-  Future getDefaultAssocaitonBanner();
+  Future<String> getDefaultAssocaitonBannerUrl();
   Future getImageByPost();
+  Future getBannerByName(String path);
+  Future uploadBannerToStorage(dynamic banner);
 }
