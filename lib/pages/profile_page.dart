2c9d6cb5a608ea66849f7f7eece72f7baef5f577
Iankk <yani_foughali@gmail.com>
2021-06-07 23:29:24 +0200

    AN-26 : remove useless redirection

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -117,7 +117,6 @@ class Profile extends StatelessWidget {
                       ),
                       onPressed: () {
                         Navigator.of(context).pop();
-                        Profile();
                       },
                     )
                   ],
