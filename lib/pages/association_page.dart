1885b8bc6b3217d4e06a13feba52b28b7d65837f
Iankk <yani_foughali@gmail.com>
2021-06-14 16:54:00 +0200

    AN-18 : add scaffold to association_page.dart

--- lib/pages/association_page.dart
+++ lib/pages/association_page.dart
@@ -5,65 +5,72 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AssociationDetails extends StatelessWidget {
+  final Association assos;
+  AssociationDetails(this.assos);
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-      FutureBuilder(
-          future: FireStoreService().getAllAssociations(),
-          builder: (ctx, AsyncSnapshot<List<Association>> snapshot) {
-            if (snapshot.hasData) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return CircularProgressIndicator();
-                case ConnectionState.done:
-                  return Column(
-                    children: [
-                      SizedBox(height: 20),
-                      AnBigTitle(snapshot.data![0].name),
-                      SizedBox(height: 30),
-                      Image.network(
-                        snapshot.data![0].banner,
-                        width: MediaQuery.of(context).size.width,
-                      )
-                    ],
-                  );
-                case ConnectionState.none:
-                  return CircularProgressIndicator();
-                case ConnectionState.active:
-                  return CircularProgressIndicator();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(assos.name),
+      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        FutureBuilder(
+            future: FireStoreService().getAllAssociations(),
+            builder: (ctx, AsyncSnapshot<List<Association>> snapshot) {
+              if (snapshot.hasData) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return CircularProgressIndicator();
+                  case ConnectionState.done:
+                    return Column(
+                      children: [
+                        SizedBox(height: 20),
+                        AnBigTitle(assos.name),
+                        SizedBox(height: 30),
+                        Image.network(
+                          assos.banner,
+                          width: MediaQuery.of(context).size.width,
+                        )
+                      ],
+                    );
+                  case ConnectionState.none:
+                    return CircularProgressIndicator();
+                  case ConnectionState.active:
+                    return CircularProgressIndicator();
+                }
               }
-            }
-            return Container();
-          }),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.add_box),
-            color: Colors.lightGreen,
-            iconSize: 30,
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.message_outlined),
-            color: Colors.lightGreen,
-            iconSize: 30,
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.date_range),
-            color: Colors.lightGreen,
-            iconSize: 30,
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.menu),
-            color: Colors.lightGreen,
-            iconSize: 30,
-            onPressed: () {},
-          ),
-        ],
-      )
-    ]);
+              return Container();
+            }),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.add_box),
+              color: Colors.lightGreen,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.message_outlined),
+              color: Colors.lightGreen,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.date_range),
+              color: Colors.lightGreen,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.menu),
+              color: Colors.lightGreen,
+              iconSize: 30,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ]),
+    );
   }
 }
