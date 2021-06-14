eb2d429d3516a4489a51bf921493caa225a52989
Alessandro Alterno <alternonolimit@gmail.com>
2021-06-14 21:29:21 +0200

    Added onTap to association_card.dart

--- lib/components/association_card.dart
+++ lib/components/association_card.dart
@@ -10,49 +10,55 @@ class AssociationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.horizontal(left: Radius.elliptical(2, 1), right: Radius.elliptical(1, 2))
-      ),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(5,10, 5, 10),
-        child: Center(
-          child: Stack(
-            children: [
-              Image.network(
-                association.banner,
-                height: double.infinity,
-                width: double.infinity,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if(loadingProgress == null) return child;
-                  else return Center(child: CircularProgressIndicator());
-                },
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      AssociationCardTitle(association),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(5, 5, 0, 5),
-                        child: Text(association.city),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(5, 5, 15, 5),
-                        child: Text(association.postalCode),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ],
+    return InkWell(
+      splashColor: Colors.teal,
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.horizontal(
+                left: Radius.elliptical(2, 1), right: Radius.elliptical(1, 2))),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+          child: Center(
+            child: Stack(
+              children: [
+                Image.network(
+                  association.banner,
+                  height: double.infinity,
+                  width: double.infinity,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null)
+                      return child;
+                    else
+                      return Center(child: CircularProgressIndicator());
+                  },
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        AssociationCardTitle(association),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5, 5, 0, 5),
+                          child: Text(association.city),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5, 5, 15, 5),
+                          child: Text(association.postalCode),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
