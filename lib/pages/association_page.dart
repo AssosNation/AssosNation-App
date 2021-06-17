c9b84601a114eec61d8cc9adb457cd5d9d6b005e
Iankk <yani_foughali@gmail.com>
2021-06-17 12:45:05 +0200

    AN-18 : add shrinkwrap at true

--- lib/pages/association_page.dart
+++ lib/pages/association_page.dart
@@ -18,169 +18,170 @@ class AssociationDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text("Association Page"),
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-        Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-              color: Colors.teal,
-              child: AnBigTitle(assos.name),
-              height: 70,
-            ),
-            Image.network(
-              assos.banner,
-              width: MediaQuery.of(context).size.width,
-            ),
-          ],
-        ),
-        Container(
-          color: Colors.teal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Column(
             children: [
-              Column(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.add_box),
-                    color: Colors.white,
-                    iconSize: 30,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              IconButton(
-                icon: const Icon(Icons.message_outlined),
-                color: Colors.white,
-                iconSize: 30,
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.date_range),
-                color: Colors.white,
-                iconSize: 30,
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.info_outline_rounded),
-                color: Colors.white,
-                iconSize: 30,
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        elevation: 50,
-                        title: Icon(
-                          Icons.info_sharp,
-                          color: Colors.teal,
-                          size: 55,
-                        ),
-                        actions: [
-                          Row(
-                            children: [
-                              const Icon(
-                                CupertinoIcons.location,
-                                color: Colors.teal,
-                              ),
-                              Text(
-                                assos.address,
-                                style: TextStyle(color: Colors.teal),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.location_city_rounded,
-                                color: Colors.teal,
-                              ),
-                              Text(
-                                assos.city,
-                                style: TextStyle(color: Colors.teal),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const Icon(
-                                CupertinoIcons.phone,
-                                color: Colors.teal,
-                              ),
-                              Text(
-                                assos.phone,
-                                style: TextStyle(color: Colors.teal),
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const Icon(CupertinoIcons.person_alt_circle,
-                                  color: Colors.teal),
-                              Text(
-                                assos.president,
-                                style: TextStyle(color: Colors.teal),
-                              )
-                            ],
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(child: DescriptionAsso(assos.description)),
-              SizedBox(height: 10),
-              Divider(
-                thickness: 3,
-                indent: 15,
-                endIndent: 15,
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
                 color: Colors.teal,
-                height: 40,
+                child: AnBigTitle(assos.name),
+                height: 70,
               ),
-              AnTitle("News Feed"),
+              Image.network(
+                assos.banner,
+                width: MediaQuery.of(context).size.width,
+              ),
             ],
           ),
-        ),
-        FutureBuilder(
-          future: FireStoreService().getAllPostsByAssociation(),
-          builder: (context, AsyncSnapshot<List<Post>> snapshot) {
-            if (snapshot.hasData) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return CircularProgressIndicator();
-                case ConnectionState.waiting:
-                  return CircularProgressIndicator();
-                case ConnectionState.active:
-                  break;
-                case ConnectionState.done:
-                  List<Post> postList = snapshot.data!;
-                  return Expanded(
-                    child: ListView.builder(
+          Container(
+            color: Colors.teal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.add_box),
+                      color: Colors.white,
+                      iconSize: 30,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                IconButton(
+                  icon: const Icon(Icons.message_outlined),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.date_range),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(Icons.info_outline_rounded),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          elevation: 50,
+                          title: Icon(
+                            Icons.info_sharp,
+                            color: Colors.teal,
+                            size: 55,
+                          ),
+                          actions: [
+                            Row(
+                              children: [
+                                const Icon(
+                                  CupertinoIcons.location,
+                                  color: Colors.teal,
+                                ),
+                                Text(
+                                  assos.address,
+                                  style: TextStyle(color: Colors.teal),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.location_city_rounded,
+                                  color: Colors.teal,
+                                ),
+                                Text(
+                                  assos.city,
+                                  style: TextStyle(color: Colors.teal),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  CupertinoIcons.phone,
+                                  color: Colors.teal,
+                                ),
+                                Text(
+                                  assos.phone,
+                                  style: TextStyle(color: Colors.teal),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Icon(CupertinoIcons.person_alt_circle,
+                                    color: Colors.teal),
+                                Text(
+                                  assos.president,
+                                  style: TextStyle(color: Colors.teal),
+                                )
+                              ],
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(child: DescriptionAsso(assos.description)),
+                SizedBox(height: 10),
+                Divider(
+                  thickness: 3,
+                  indent: 15,
+                  endIndent: 15,
+                  color: Colors.teal,
+                  height: 40,
+                ),
+                AnTitle("News Feed"),
+              ],
+            ),
+          ),
+          FutureBuilder(
+            future: FireStoreService().getAllPostsByAssociation(),
+            builder: (context, AsyncSnapshot<List<Post>> snapshot) {
+              if (snapshot.hasData) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                    return CircularProgressIndicator();
+                  case ConnectionState.waiting:
+                    return CircularProgressIndicator();
+                  case ConnectionState.active:
+                    break;
+                  case ConnectionState.done:
+                    List<Post> postList = snapshot.data!;
+                    return ListView.builder(
+                      shrinkWrap: true,
                       itemBuilder: (context, index) {
                         return NewsFeedCard(postList[index]);
                       },
                       itemCount: postList.length,
-                    ),
-                  );
+                    );
+                }
               }
-            }
-            if (snapshot.hasError) {
-              return Container(
-                child: Text("Something wrong happened"),
-              );
-            }
-            return Container();
-          },
-        ),
-      ]),
+              if (snapshot.hasError) {
+                return Container(
+                  child: Text("Something wrong happened"),
+                );
+              }
+              return Container();
+            },
+          ),
+        ]),
+      ),
     );
   }
 }
