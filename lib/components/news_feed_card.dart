1eddd7a4b27eb00e9094c391196fe15e24cde8f3
Alessandro Alterno <alternonolimit@gmail.com>
2021-06-20 03:53:47 +0200

    Fix typo bug

--- lib/components/news_feed_card.dart
+++ lib/components/news_feed_card.dart
@@ -1,6 +1,5 @@
 import 'package:assosnation_app/services/firebase/firestore/firestore_service.dart';
 import 'package:assosnation_app/services/firebase/storage/storage_service.dart';
-import 'package:assosnation_app/services/models/post.dart';
 import 'package:assosnation_app/services/models/user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +8,7 @@ import 'package:provider/provider.dart';
 import 'forms/form_main_title.dart';
 
 class NewsFeedCard extends StatefulWidget {
-  Post _post;
+  final _post;
   NewsFeedCard(this._post);
 
   @override
@@ -100,7 +99,7 @@ class _NewsFeedCardState extends State<NewsFeedCard> {
                         color: Theme.of(context).accentColor,
                       ),
                     ),
-                    Text(this.widget._post.likesNumber.toString()),
+                    Text(this.widget._post.usersWhoLiked.length.toString()),
                   ],
                 ),
                 Divider(),
@@ -110,7 +109,7 @@ class _NewsFeedCardState extends State<NewsFeedCard> {
                     TextButton.icon(
                         onPressed: () {
                           if (_user != null) {
-                            if (widget._post.userLiked) {
+                            if (widget._post.didUserLikeThePost(_user.uid)) {
                               FireStoreService().removeUserToLikedList(
                                   widget._post.id, _user.uid);
                             } else {
@@ -119,7 +118,7 @@ class _NewsFeedCardState extends State<NewsFeedCard> {
                             }
                           }
                         },
-                        icon: Icon(widget._post.userLiked
+                        icon: Icon(widget._post.didUserLikeThePost(_user!.uid)
                             ? Icons.thumb_up_alt_rounded
                             : Icons.thumb_up_alt_outlined),
                         label: Text("Like")),
