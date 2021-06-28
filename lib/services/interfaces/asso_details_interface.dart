15c6a81b17e912b0c2993d2101634935fed32937
Iankk <yani_foughali@gmail.com>
2021-06-28 17:39:22 +0200

    AN-34 : an asso can now edit all its infos

--- lib/services/interfaces/asso_details_interface.dart
+++ lib/services/interfaces/asso_details_interface.dart
@@ -3,4 +3,7 @@ import 'package:assosnation_app/services/models/association.dart';
 abstract class AssoDetailsInterface {
   Future updateName(Association assos, String content);
   Future updatePhone(Association assos, String content);
+  Future updateAddress(Association assos, String content);
+  Future updateCity(Association assos, String content);
+  Future updateDescription(Association assos, String content);
 }
