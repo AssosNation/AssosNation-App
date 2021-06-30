6684ae745bab09200aa5d0c6cfeb62afc0a870e6
Lukas Brasseleur <l.brasseleur@cafpi.fr>
2021-06-30 03:42:57 +0200

    Update actions to make participate action work

--- lib/services/firebase/firestore/firestore_service.dart
+++ lib/services/firebase/firestore/firestore_service.dart
@@ -305,14 +305,23 @@ class FireStoreService extends DatabaseInterface {
     }
   }
 
-  addUserToAction(associationId, action, user) async {
-    Association association =
-        await this.getAssociationInfosFromDB(associationId);
-    association.actions![action]['usersRegistered'].add(user);
+  addUserToAction(AssociationAction action, _userId) async {
+    Association association = action.association;
+    association.actions![action.id]['usersRegistered'].add(_userId);
     _service
         .collection('associations')
-        .doc(associationId)
-        .update({"actions": FieldValue.arrayUnion(association.actions!)});
+        .doc(association.uid)
+        .update({"actions": association.actions});
+  }
+
+  removeUserToAction(AssociationAction action, _userId) async {
+    Association association = action.association;
+    association.actions![action.id]['usersRegistered']
+        .removeWhere((userId) => userId == _userId);
+    _service
+        .collection('associations')
+        .doc(association.uid)
+        .update({"actions": association.actions});
   }
 
   /**
