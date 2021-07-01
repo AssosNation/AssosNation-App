6a6bc5c31bdede320b3b9e3500d795253c1eb853
Alessandro Alterno <alternonolimit@gmail.com>
2021-07-01 22:07:54 +0200

    Added Perf monitoring for Android

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:assosnation_app/utils/association_search.dart';
 import 'package:assosnation_app/utils/constants.dart';
 import 'package:assosnation_app/utils/route_generator.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -18,43 +17,50 @@ import 'package:provider/provider.dart';
 
 import 'l10n/l10n.dart';
 
-void main() async {
-  await Firebase.initializeApp();
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    return MultiProvider(
-      providers: [
-        StreamProvider<AnUser?>(
-          create: (context) => AuthService().user,
-          initialData: null,
-        ),
-        StreamProvider<Association?>(
-          create: (context) => AuthService().association,
-          initialData: null,
-        ),
-      ],
-      builder: (context, child) => MaterialApp(
-        title: Constants.appName,
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
-          textTheme: GoogleFonts.montserratTextTheme(
-            Theme.of(context).textTheme,
-          ),
-        ),
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: L10n.all,
-        home: MyHomePage(
-          key: UniqueKey(),
-        ),
-        initialRoute: "/",
-        onGenerateRoute: RouteGenerator.generateRoute,
-      ),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MultiProvider(
+            providers: [
+              StreamProvider<AnUser?>(
+                create: (context) => AuthService().user,
+                initialData: null,
+              ),
+              StreamProvider<Association?>(
+                create: (context) => AuthService().association,
+                initialData: null,
+              ),
+            ],
+            builder: (context, child) => MaterialApp(
+              title: Constants.appName,
+              theme: ThemeData(
+                primarySwatch: Colors.teal,
+                textTheme: GoogleFonts.montserratTextTheme(
+                  Theme.of(context).textTheme,
+                ),
+              ),
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: L10n.all,
+              home: MyHomePage(
+                key: UniqueKey(),
+              ),
+              initialRoute: "/",
+              onGenerateRoute: RouteGenerator.generateRoute,
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.none) {
+          return CircularProgressIndicator();
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
 }
