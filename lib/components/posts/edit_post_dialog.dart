cbc4d9b140a8d6be673642ab9c7b0444a88ad182
Alessandro Alterno <alternonolimit@gmail.com>
2021-07-02 20:47:09 +0200

    Can now add and update existing images on posts

--- lib/components/posts/edit_post_dialog.dart
+++ lib/components/posts/edit_post_dialog.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+
+import 'package:assosnation_app/components/forms/form_subtitle.dart';
 import 'package:assosnation_app/components/posts/post_main_subtitle.dart';
 import 'package:assosnation_app/components/posts/post_main_title.dart';
 import 'package:assosnation_app/services/firebase/firestore/posts_service.dart';
+import 'package:assosnation_app/services/firebase/storage/storage_service.dart';
 import 'package:assosnation_app/services/models/post.dart';
+import 'package:assosnation_app/utils/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../no_image_placeholder.dart';
 
 class EditPostDialog extends StatefulWidget {
   final Post _post;
@@ -19,43 +26,60 @@ class _EditPostDialogState extends State<EditPostDialog> {
 
   String _title = "";
   String _content = "";
+  File? _updatedImage;
 
   _verifyAndValidateForm() async {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         dynamic res =
             await PostService().updatePost(widget._post, _title, _content);
+        if (_updatedImage != null)
+          await StorageService()
+              .uploadPostImageToStorage(_updatedImage!, widget._post.id);
         if (res == true) {
           Navigator.pop(context);
-          _displaySnackBarWithMessage(
-              "Your post has been updated", Colors.green);
+          Utils.displaySnackBarWithMessage(
+              context, "Your post has been updated", Colors.green);
         } else {
           Navigator.pop(context);
-          _displaySnackBarWithMessage(
+          Utils.displaySnackBarWithMessage(context,
               "Something wrong happened, please try again", Colors.red);
         }
       }
     }
   }
 
-  _displaySnackBarWithMessage(String msg, Color color) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      behavior: SnackBarBehavior.floating,
-      content: Text(msg),
-      backgroundColor: color,
-      duration: Duration(seconds: 3),
-    ));
+  Widget _displaySelectedImage() {
+    if (_updatedImage != null) {
+      return SizedBox(
+          height: MediaQuery.of(context).size.height * 0.3,
+          width: MediaQuery.of(context).size.width,
+          child: Image.file(
+            _updatedImage!,
+            fit: BoxFit.fill,
+          ));
+    } else if (widget._post.photo != "") {
+      return SizedBox(
+          height: MediaQuery.of(context).size.height * 0.3,
+          width: MediaQuery.of(context).size.width,
+          child: Image.network(
+            widget._post.photo,
+            fit: BoxFit.fill,
+          ));
+    } else {
+      return NoImagePlaceholder();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.4,
-      child: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Expanded(
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -83,7 +107,35 @@ class _EditPostDialogState extends State<EditPostDialog> {
                   ],
                 ),
                 Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FormSubTitle("Image : "),
+                    Expanded(
+                      child: InkWell(
+                        splashColor: Theme.of(context).accentColor,
+                        onTap: () async {
+                          final img =
+                              await StorageService().selectImageFromGallery();
+                          setState(() {
+                            _updatedImage = img;
+                          });
+                        },
+                        child: Stack(
+                          children: [
+                            _displaySelectedImage(),
+                            Positioned.fill(
+                                child: Icon(
+                              Icons.add_a_photo_outlined,
+                              color: Theme.of(context).accentColor,
+                            )),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     PostMainSubtitle("Content : "),
                     Expanded(
